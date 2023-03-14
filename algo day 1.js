289440fff50bde490696b1648f4ef29921b33a37
Miles Kerr <m.kerr.professional@gmail.com>
2023-03-14 11:05:21 -0500

    day 1 algo

---
+++ algo day 1.js
@@ -0,0 +1,13 @@
+// Lesson 3: Predict what the code does
+function getTotal(arrayOfNumbers) {
+    var sum = arrayOfNumbers[0];
+
+    for (var i = 0; i < arrayOfNumbers.length; i++) {
+        sum += arrayOfNumbers[i];
+        console.log("the current sum is: " + sum);
+    }
+
+    console.log("the total is: " + sum);
+}
+
+getTotal([1, 3, 5]);
